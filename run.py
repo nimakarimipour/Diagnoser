7672a2eb8481ded68032c92ace85a0b944859802
nimakarimipour <karimipour.nima@gmail.com>
2021-04-13 23:54:32 -0700

    update

--- run.py
+++ run.py
@@ -111,7 +111,7 @@ def apply():
     reports = json.load(report_file)
     cleaned = {}
     uprint("Selecting effective fixes...")
-    cleaned['fixes'] = [fix for fix in reports['reports'] if fix['jump'] < 1 and (not fix['class'].endswith("BeanCurrentlyInCreationFailureAnalyzer"))]
+    cleaned['fixes'] = [fix for fix in reports['reports'] if fix['jump'] < 1]
     uprint("Selected effective fixes.")
     with open(out_dir + "/cleaned.json", 'w') as outfile:
         json.dump(cleaned, outfile)
