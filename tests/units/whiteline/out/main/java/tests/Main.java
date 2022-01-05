25a452048d369e4b4a768fa23413df124d9dce3d
nimakarimipour <karimipour.nima@gmail.com>
2022-01-05 12:44:40 -0600

    updated test.

--- tests/units/whiteline/out/main/java/tests/Main.java
+++ tests/units/whiteline/out/main/java/tests/Main.java
@@ -2,14 +2,29 @@ package tests;
 import javax.annotation.Nullable;
 
 
-
-
-
-
 public class Main {
-    @Nullable
-    public Object run(){
 
+
+    Object field2 = new Object();
+    public Object returnNullable(){
         return null;
+    }
+
+    public void useNullable1(Object p){
+        Integer i = p.hashCode();
+    }
+
+    public void useNullable2(Object p){
+        Integer i = p.hashCode();
+    }
+
+    public void useNullable3(Object p){
+        Integer i = p.hashCode();
+    }
+
+    public void run(){
+        useNullable1(returnNullable());
+        useNullable2(returnNullable());
+        useNullable3(returnNullable());
     }
 }
--- tests/units/whiteline/src/main/java/tests/Main.java
+++ tests/units/whiteline/src/main/java/tests/Main.java
@@ -1,11 +1,28 @@
 package tests;
 
-
-
-
-
 public class Main {
-    public Object run(){
+
+
+    Object field2 = new Object();
+    public Object returnNullable(){
         return null;
+    }
+
+    public void useNullable1(Object p){
+        Integer i = p.hashCode();
+    }
+
+    public void useNullable2(Object p){
+        Integer i = p.hashCode();
+    }
+
+    public void useNullable3(Object p){
+        Integer i = p.hashCode();
+    }
+
+    public void run(){
+        useNullable1(returnNullable());
+        useNullable2(returnNullable());
+        useNullable3(returnNullable());
     }
 }
